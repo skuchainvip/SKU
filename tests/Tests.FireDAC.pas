ea82f01477cfda0fb8eaf02bf08d975106dce9ba
logo <38019049+skuchainvip@users.noreply.github.com>
2021-04-24 10:19:47 +0800

    Add files via upload

---
+++ tests/Tests.FireDAC.pas
@@ -0,0 +1,250 @@
+unit Tests.FireDAC;
+
+interface
+
+uses
+  Classes, SysUtils, Rtti, Types
+, DUnitX.TestFramework
+, FireDAC.Comp.Client, FireDAC.Comp.DataSet, FireDAC.VCLUI.Wait, FireDAC.Phys.SQLite
+, Skuchain.Core.MediaType
+, Skuchain.Core.MessageBodyReader, Skuchain.Core.MessageBodyWriter
+, Skuchain.Core.MessageBodyReaders, Skuchain.Core.MessageBodyWriters
+, Skuchain.Data.FireDAC
+, Skuchain.Data.FireDAC.ReadersAndWriters
+, Skuchain.Data.FireDAC.Utils
+, Skuchain.Core.JSON
+;
+
+type
+  [TestFixture('MBW_FireDAC')]
+  TSkuchainFireDACReaderWriterTest = class(TObject)
+  private
+    FJSONMediaType: TMediaType;
+    FOutputStream: TStringStream;
+    FDConnection: TFDConnection;
+    FDQuery1, FDQuery2, FDQuery3: TFDCustomQuery;
+  protected
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure ArrayOfDataSets;
+
+    [Test]
+    procedure ArrayOfDataSetsInTValue;
+
+    [Test]
+    procedure ParamsInjection;
+  end;
+
+implementation
+
+uses
+  IOUtils, Data.DB,
+  Skuchain.Core.Activation.Interfaces
+;
+
+{ TSkuchainFireDACWriterTest }
+
+procedure TSkuchainFireDACReaderWriterTest.ArrayOfDataSets;
+var
+  LDataSet: TFDMemTable;
+  LDataSets: TArray<TFDMemTable>;
+  LName: string;
+  LData: TFDAdaptedDataSet;
+begin
+  TFDDataSets.ToJSON([FDQuery1, FDQuery2, FDQuery3], FOutputStream);
+
+  LDataSets := TFDDataSets.FromJSON(FOutputStream);
+  Assert.AreEqual(3, Length(LDataSets));
+  try
+    for LDataSet in LDataSets do
+    begin
+      LName := LDataSet.Name;
+      LData := LDataSet;
+
+      if SameText(LName, FDQuery1.Name) then
+      begin
+        Assert.AreEqual(FDQuery1.RecordCount, LData.RecordCount);
+        Assert.AreEqual(FDQuery1.Fields.Count, LData.Fields.Count);
+        Assert.AreEqual(FDQuery1.Fields[0].FieldName, LData.Fields[0].FieldName);
+
+        FDQuery1.First;
+        LData.First;
+        Assert.AreEqual(FDQuery1.Fields[0].Value, LData.Fields[0].Value);
+      end
+      else if SameText(LName, FDQuery2.Name) then
+      begin
+        Assert.AreEqual(FDQuery2.RecordCount, LData.RecordCount);
+        Assert.AreEqual(FDQuery2.Fields.Count, LData.Fields.Count);
+        Assert.AreEqual(FDQuery2.Fields[0].FieldName, LData.Fields[0].FieldName);
+
+        FDQuery2.First;
+        LData.First;
+        Assert.AreEqual(FDQuery2.Fields[0].Value, LData.Fields[0].Value);
+      end
+      else if SameText(LName, FDQuery3.Name) then
+      begin
+        Assert.AreEqual(FDQuery3.RecordCount, LData.RecordCount);
+        Assert.AreEqual(FDQuery3.Fields.Count, LData.Fields.Count);
+        Assert.AreEqual(FDQuery3.Fields[0].FieldName, LData.Fields[0].FieldName);
+
+        FDQuery3.First;
+        LData.First;
+        Assert.AreEqual(FDQuery3.Fields[0].Value, LData.Fields[0].Value);
+      end;
+    end;
+
+  finally
+    TFDDatasets.FreeAll(LDataSets);
+  end;
+end;
+
+procedure TSkuchainFireDACReaderWriterTest.ArrayOfDataSetsInTValue;
+var
+  LDataSet: TFDMemTable;
+  LDataSets: TArray<TFDMemTable>;
+  LName: string;
+  LData: TFDAdaptedDataSet;
+begin
+  TFDDataSets.ToJSON(
+    TValue.From<TArray<TFDCustomQuery>>([FDQuery1, FDQuery2, FDQuery3])
+    , FOutputStream);
+
+  LDataSets := TFDDataSets.FromJSON(FOutputStream);
+  Assert.AreEqual(3, Length(LDataSets));
+  try
+    for LDataSet in LDataSets do
+    begin
+      LName := LDataSet.Name;
+      LData := LDataSet;
+
+      if SameText(LName, FDQuery1.Name) then
+      begin
+        Assert.AreEqual(FDQuery1.RecordCount, LData.RecordCount);
+        Assert.AreEqual(FDQuery1.Fields.Count, LData.Fields.Count);
+        Assert.AreEqual(FDQuery1.Fields[0].FieldName, LData.Fields[0].FieldName);
+
+        FDQuery1.First;
+        LData.First;
+        Assert.AreEqual(FDQuery1.Fields[0].Value, LData.Fields[0].Value);
+      end
+      else if SameText(LName, FDQuery2.Name) then
+      begin
+        Assert.AreEqual(FDQuery2.RecordCount, LData.RecordCount);
+        Assert.AreEqual(FDQuery2.Fields.Count, LData.Fields.Count);
+        Assert.AreEqual(FDQuery2.Fields[0].FieldName, LData.Fields[0].FieldName);
+
+        FDQuery2.First;
+        LData.First;
+        Assert.AreEqual(FDQuery2.Fields[0].Value, LData.Fields[0].Value);
+      end
+      else if SameText(LName, FDQuery3.Name) then
+      begin
+        Assert.AreEqual(FDQuery3.RecordCount, LData.RecordCount);
+        Assert.AreEqual(FDQuery3.Fields.Count, LData.Fields.Count);
+        Assert.AreEqual(FDQuery3.Fields[0].FieldName, LData.Fields[0].FieldName);
+
+        FDQuery3.First;
+        LData.First;
+        Assert.AreEqual(FDQuery3.Fields[0].Value, LData.Fields[0].Value);
+      end;
+    end;
+
+  finally
+    TFDDatasets.FreeAll(LDataSets);
+  end;
+end;
+
+procedure TSkuchainFireDACReaderWriterTest.ParamsInjection;
+var
+  LFD: TSkuchainFireDAC;
+  LQuery: TFDQuery;
+begin
+  LFD := TSkuchainFireDAC.Create('MAIN_DB');
+  try
+    LQuery := LFD.CreateQuery('select * from EMPLOYEE where EMP_NO = :Custom_VALUE1', nil, False);
+    try
+      LFD.AddContextValueProvider(
+        procedure (const AActivation: ISkuchainActivation;
+        const AName: string; const ADesiredType: TFieldType; out AValue: TValue)
+        begin
+          if SameText(AName, 'Custom_VALUE1') then
+            AValue := 123;
+        end
+      );
+      LFD.InjectParamValues(LQuery.Command);
+      Assert.AreEqual(123, LQuery.ParamByName('Custom_VALUE1').AsInteger);
+    finally
+      LQuery.Free;
+    end;
+  finally
+    LFD.Free;
+  end;
+end;
+
+procedure TSkuchainFireDACReaderWriterTest.Setup;
+begin
+  FJSONMediaType := TMediaType.Create(TMediaType.APPLICATION_JSON);
+  Assert.IsNotNull(FJSONMediaType);
+
+  FOutputStream := TStringStream.Create;
+
+  FDConnection := TFDConnection.Create(nil);
+  try
+    FDConnection.ConnectionDefName := 'SQLite_Demo';
+    FDConnection.Connected := True;
+    Assert.IsTrue(FDConnection.Connected);
+  except
+    FreeAndNil(FDConnection);
+    raise;
+  end;
+
+  FDQuery1 := TFDQuery.Create(nil);
+  try
+    FDQuery1.Connection := FDConnection;
+    FDQuery1.Open('select * from EMPLOYEES');
+    FDQuery1.Name := 'EmployeesQuery';
+    Assert.IsFalse(FDQuery1.IsEmpty, 'FDQuery1 data unavailable');
+  except
+    FreeAndNil(FDQuery1);
+    raise;
+  end;
+
+  FDQuery2 := TFDQuery.Create(nil);
+  try
+    FDQuery2.Connection := FDConnection;
+    FDQuery2.Open('select * from CUSTOMERS');
+    FDQuery2.Name := 'CustomersQuery';
+    Assert.IsFalse(FDQuery2.IsEmpty, 'FDQuery2 data unavailable');
+  except
+    FreeAndNil(FDQuery2);
+    raise;
+  end;
+
+  FDQuery3 := TFDQuery.Create(nil);
+  try
+    FDQuery3.Connection := FDConnection;
+    FDQuery3.Open('select * from CATEGORIES');
+    FDQuery3.Name := 'CategoriesQuery';
+    Assert.IsFalse(FDQuery3.IsEmpty, 'FDQuery3 data unavailable');
+  except
+    FreeAndNil(FDQuery3);
+    raise;
+  end;
+
+end;
+
+procedure TSkuchainFireDACReaderWriterTest.TearDown;
+begin
+  FJSONMediaType.Free;
+  FOutputStream.Free;
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TSkuchainFireDACReaderWriterTest);
+
+end.
---
+++ tests/Tests.MessageBodyWriters.pas
@@ -0,0 +1,426 @@
+unit Tests.MessageBodyWriters;
+
+interface
+
+uses
+  Classes, SysUtils
+, DUnitX.TestFramework
+, Skuchain.Core.MediaType
+, Skuchain.Core.MessageBodyWriter
+, Skuchain.Core.MessageBodyWriters
+;
+
+type
+  [TestFixture('MBW_Records')]
+  TSkuchainRecordWriterTest = class(TObject)
+  private
+    FMBW: IMessageBodyWriter;
+    FJSONMediaType: TMediaType;
+    FOutputStream: TStringStream;
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure Simple;
+
+    [Test]
+    procedure PrimitiveTypes;
+
+    [Test]
+    procedure Nested;
+  end;
+
+  [TestFixture('MBW_Records')]
+  TSkuchainArrayOfRecordWriterTest = class(TObject)
+  private
+    FMBW: IMessageBodyWriter;
+    FJSONMediaType: TMediaType;
+    FOutputStream: TStringStream;
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure Empty;
+
+    [Test]
+    procedure SingleItem;
+
+    [Test]
+    procedure Items;
+
+    [Test]
+    procedure NestedRecords;
+
+    [Test]
+    procedure NestedArrays;
+  end;
+
+  function GetRecordMBW: IMessageBodyWriter;
+  function GetArrayOfRecordMBW: IMessageBodyWriter;
+
+implementation
+
+uses
+  DateUtils, Rtti
+, Skuchain.Core.JSON
+, Tests.Records.Types
+;
+
+function GetRecordMBW: IMessageBodyWriter;
+begin
+  Result := TRecordWriter.Create as IMessageBodyWriter;
+end;
+
+function GetArrayOfRecordMBW: IMessageBodyWriter;
+begin
+  Result := TArrayOfRecordWriter.Create as IMessageBodyWriter;
+end;
+
+
+{ TSkuchainRecordWriterTest }
+
+procedure TSkuchainRecordWriterTest.Setup;
+begin
+  FMBW := GetRecordMBW;
+  Assert.IsNotNull(FMBW);
+
+  FJSONMediaType := TMediaType.Create(TMediaType.APPLICATION_JSON);
+  Assert.IsNotNull(FJSONMediaType);
+
+  FOutputStream := TStringStream.Create;
+end;
+
+procedure TSkuchainRecordWriterTest.TearDown;
+begin
+  FMBW := nil;
+  FJSONMediaType.Free;
+  FOutputStream.Free;
+end;
+
+procedure TSkuchainRecordWriterTest.Nested;
+var
+  LRecordValue: TValue;
+  LOuterObj: TJSONObject;
+  LInnerObj: TJSONObject;
+begin
+  LRecordValue := TValue.From<TOuterRecord>(TOuterRecord.Create('Outer', 'Inner', 123));
+
+  FMBW.WriteTo(LRecordValue, FJSONMediaType, FOutputStream, nil);
+
+  LOuterObj := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONObject;
+  try
+    Assert.IsNotNull(LOuterObj);
+
+    Assert.AreEqual('Outer', LOuterObj.ReadStringValue('Name'));
+
+    LInnerObj := LOuterObj.Get('Inner').JsonValue as TJSONObject;
+    Assert.IsNotNull(LInnerObj);
+
+    Assert.AreEqual('Inner', LInnerObj.ReadStringValue('Name'));
+    Assert.AreEqual(123, LInnerObj.ReadIntegerValue('Value'));
+  finally
+    LOuterObj.Free;
+  end;
+end;
+
+procedure TSkuchainRecordWriterTest.PrimitiveTypes;
+var
+  LRecord: TPrimitiveTypesRecord;
+  LRecordValue: TValue;
+  LJSONObj: TJSONObject;
+begin
+  LRecord.AString := 'Andrea';
+  LRecord.ABoolean := True;
+  LRecord.AInteger := 123;
+  LRecord.AFloat := 1234.56789;
+  LRecord.ACurrency := 7.75;
+  LRecord.ADate := EncodeDate(1982, 5, 24);
+  LRecord.AChar := 'C';
+
+  LRecordValue := TValue.From<TPrimitiveTypesRecord>(LRecord);
+
+  FMBW.WriteTo(LRecordValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONObj := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONObject;
+  try
+    Assert.IsNotNull(LJSONObj);
+
+    Assert.AreEqual('Andrea', LJSONObj.ReadStringValue('AString'));
+    Assert.AreEqual(True, LJSONObj.ReadBoolValue('ABoolean'));
+    Assert.AreEqual(123, LJSONObj.ReadIntegerValue('AInteger'));
+    Assert.AreEqual(Double(1234.56789), LJSONObj.ReadDoubleValue('AFloat'));
+    Assert.AreEqual<Currency>(Currency(7.75), Currency(LJSONObj.ReadDoubleValue('ACurrency')));
+    Assert.AreEqual(EncodeDate(1982, 5, 24), LJSONObj.ReadDateTimeValue('ADate'));
+    Assert.AreEqual('C', LJSONObj.ReadStringValue('AChar'));
+  finally
+    LJSONObj.Free;
+  end;
+end;
+
+procedure TSkuchainRecordWriterTest.Simple;
+var
+  LRecord: TPersonRecord;
+  LRecordValue: TValue;
+  LJSONObj: TJSONObject;
+begin
+  LRecord.Name := 'Andrea';
+  LRecord.Surname := 'Magni';
+  LRecord.DateOfBirth := EncodeDate(1982, 5, 24);
+  LRecordValue := TValue.From<TPersonRecord>(LRecord);
+
+  FMBW.WriteTo(LRecordValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONObj := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONObject;
+  try
+    Assert.IsNotNull(LJSONObj);
+
+    Assert.AreEqual('Andrea', LJSONObj.ReadStringValue('Name'));
+    Assert.AreEqual('Magni', LJSONObj.ReadStringValue('Surname'));
+    Assert.AreEqual(EncodeDate(1982, 5, 24), LJSONObj.ReadDateTimeValue('DateOfBirth'));
+  finally
+    LJSONObj.Free;
+  end;
+end;
+
+{ TSkuchainArrayOfRecordWriterTest }
+
+procedure TSkuchainArrayOfRecordWriterTest.Setup;
+begin
+  FMBW := GetArrayOfRecordMBW;
+  Assert.IsNotNull(FMBW);
+
+  FJSONMediaType := TMediaType.Create(TMediaType.APPLICATION_JSON);
+  Assert.IsNotNull(FJSONMediaType);
+
+  FOutputStream := TStringStream.Create;
+end;
+
+procedure TSkuchainArrayOfRecordWriterTest.TearDown;
+begin
+  FMBW := nil;
+  FJSONMediaType.Free;
+  FOutputStream.Free;
+end;
+
+procedure TSkuchainArrayOfRecordWriterTest.Empty;
+var
+  LValue: TValue;
+  LJSONArr: TJSONArray;
+begin
+  LValue := TValue.From<TArray<TPersonRecord>>([]);
+
+  FMBW.WriteTo(LValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONArr := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONArray;
+  try
+    Assert.IsNotNull(LJSONArr);
+
+    Assert.AreEqual(0, LJSONArr.Count);
+  finally
+    LJSONArr.Free;
+  end;
+end;
+
+procedure TSkuchainArrayOfRecordWriterTest.Items;
+var
+  LValue: TValue;
+  LJSONArr: TJSONArray;
+  LJSONObj: TJSONObject;
+begin
+  LValue := TValue.From<TArray<TNamedIntegerRecord>>([
+    TNamedIntegerRecord.Create('One', 1)
+  , TNamedIntegerRecord.Create('Two', 2)
+  , TNamedIntegerRecord.Create('Three', 3)
+  ]);
+
+  FMBW.WriteTo(LValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONArr := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONArray;
+  try
+    Assert.IsNotNull(LJSONArr);
+
+    Assert.AreEqual(3, LJSONArr.Count);
+
+    LJSONObj := LJSONArr.Items[0] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('One', LJSONObj.ReadStringValue('Name'));
+    Assert.AreEqual(1, LJSONObj.ReadIntegerValue('Value'));
+
+    LJSONObj := LJSONArr.Items[1] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Two', LJSONObj.ReadStringValue('Name'));
+    Assert.AreEqual(2, LJSONObj.ReadIntegerValue('Value'));
+
+    LJSONObj := LJSONArr.Items[2] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Three', LJSONObj.ReadStringValue('Name'));
+    Assert.AreEqual(3, LJSONObj.ReadIntegerValue('Value'));
+  finally
+    LJSONArr.Free;
+  end;
+end;
+
+procedure TSkuchainArrayOfRecordWriterTest.NestedArrays;
+var
+  LData2, LData3: TArrayNamedIntegerRecord;
+  LValue: TValue;
+  LJSONArr: TJSONArray;
+  LJSONObj: TJSONObject;
+  LDataArr: TJSONArray;
+  LItemObj: TJSONObject;
+begin
+  LData2.Name := 'Two integers';
+  LData2.Data := [
+    TNamedIntegerRecord.Create('One', 1)
+  , TNamedIntegerRecord.Create('Two', 2)
+  ];
+
+  LData3.Name := 'Three integers';
+  LData3.Data := [
+    TNamedIntegerRecord.Create('One', 1)
+  , TNamedIntegerRecord.Create('Two', 2)
+  , TNamedIntegerRecord.Create('Three', 3)
+  ];
+
+  LValue := TValue.From<TArray<TArrayNamedIntegerRecord>>([LData2, LData3]);
+
+  FMBW.WriteTo(LValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONArr := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONArray;
+  try
+    Assert.IsNotNull(LJSONArr);
+
+    Assert.AreEqual(2, LJSONArr.Count);
+
+    // first TArrayNamedIntegerRecord item
+    LJSONObj := LJSONArr.Items[0] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Two integers', LJSONObj.ReadStringValue('Name'));
+
+    LDataArr := LJSONObj.GetValue('Data') as TJSONArray;
+    Assert.IsNotNull(LDataArr);
+    Assert.AreEqual(2, LDataArr.Count);
+    // Data items
+    LItemObj := LDataArr.Items[0] as TJSONObject;
+    Assert.IsNotNull(LItemObj);
+    Assert.AreEqual('One', LItemObj.ReadStringValue('Name'));
+    Assert.AreEqual(1, LItemObj.ReadIntegerValue('Value'));
+    LItemObj := LDataArr.Items[1] as TJSONObject;
+    Assert.IsNotNull(LItemObj);
+    Assert.AreEqual('Two', LItemObj.ReadStringValue('Name'));
+    Assert.AreEqual(2, LItemObj.ReadIntegerValue('Value'));
+
+    // second TArrayNamedIntegerRecord item
+    LJSONObj := LJSONArr.Items[1] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Three integers', LJSONObj.ReadStringValue('Name'));
+
+    LDataArr := LJSONObj.GetValue('Data') as TJSONArray;
+    Assert.IsNotNull(LDataArr);
+    Assert.AreEqual(3, LDataArr.Count);
+    // Data items
+    LItemObj := LDataArr.Items[0] as TJSONObject;
+    Assert.IsNotNull(LItemObj);
+    Assert.AreEqual('One', LItemObj.ReadStringValue('Name'));
+    Assert.AreEqual(1, LItemObj.ReadIntegerValue('Value'));
+
+    LItemObj := LDataArr.Items[1] as TJSONObject;
+    Assert.IsNotNull(LItemObj);
+    Assert.AreEqual('Two', LItemObj.ReadStringValue('Name'));
+    Assert.AreEqual(2, LItemObj.ReadIntegerValue('Value'));
+
+    LItemObj := LDataArr.Items[2] as TJSONObject;
+    Assert.IsNotNull(LItemObj);
+    Assert.AreEqual('Three', LItemObj.ReadStringValue('Name'));
+    Assert.AreEqual(3, LItemObj.ReadIntegerValue('Value'));
+  finally
+    LJSONArr.Free;
+  end;
+end;
+
+procedure TSkuchainArrayOfRecordWriterTest.NestedRecords;
+var
+  LValue: TValue;
+  LJSONArr: TJSONArray;
+  LJSONObj: TJSONObject;
+  LInnerObj: TJSONObject;
+begin
+  LValue := TValue.From<TArray<TOuterRecord>>([
+    TOuterRecord.Create('First', 'One', 1)
+  , TOuterRecord.Create('Second', 'Two', 2)
+  , TOuterRecord.Create('Third', 'Three', 3)
+  ]);
+
+  FMBW.WriteTo(LValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONArr := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONArray;
+  try
+    Assert.IsNotNull(LJSONArr);
+
+    Assert.AreEqual(3, LJSONArr.Count);
+
+    LJSONObj := LJSONArr.Items[0] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('First', LJSONObj.ReadStringValue('Name'));
+    LInnerObj := LJSONObj.GetValue('Inner') as TJSONObject;
+    Assert.IsNotNull(LInnerObj);
+    Assert.AreEqual('One', LInnerObj.ReadStringValue('Name'));
+    Assert.AreEqual(1, LInnerObj.ReadIntegerValue('Value'));
+
+    LJSONObj := LJSONArr.Items[1] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Second', LJSONObj.ReadStringValue('Name'));
+    LInnerObj := LJSONObj.GetValue('Inner') as TJSONObject;
+    Assert.IsNotNull(LInnerObj);
+    Assert.AreEqual('Two', LInnerObj.ReadStringValue('Name'));
+    Assert.AreEqual(2, LInnerObj.ReadIntegerValue('Value'));
+
+    LJSONObj := LJSONArr.Items[2] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Third', LJSONObj.ReadStringValue('Name'));
+    LInnerObj := LJSONObj.GetValue('Inner') as TJSONObject;
+    Assert.IsNotNull(LInnerObj);
+    Assert.AreEqual('Three', LInnerObj.ReadStringValue('Name'));
+    Assert.AreEqual(3, LInnerObj.ReadIntegerValue('Value'));
+  finally
+    LJSONArr.Free;
+  end;
+end;
+
+procedure TSkuchainArrayOfRecordWriterTest.SingleItem;
+var
+  LValue: TValue;
+  LJSONArr: TJSONArray;
+  LJSONObj: TJSONObject;
+begin
+  LValue := TValue.From<TArray<TPersonRecord>>([TPersonRecord.Create('Andrea', 'Magni', EncodeDate(1982, 5, 24))]);
+
+  FMBW.WriteTo(LValue, FJSONMediaType, FOutputStream, nil);
+
+  LJSONArr := TJSONObject.ParseJSONValue(FOutputStream.DataString) as TJSONArray;
+  try
+    Assert.IsNotNull(LJSONArr);
+
+    Assert.AreEqual(1, LJSONArr.Count);
+
+    LJSONObj := LJSONArr.Items[0] as TJSONObject;
+    Assert.IsNotNull(LJSONObj);
+    Assert.AreEqual('Andrea', LJSONObj.ReadStringValue('Name'));
+    Assert.AreEqual('Magni', LJSONObj.ReadStringValue('Surname'));
+  finally
+    LJSONArr.Free;
+  end;
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TSkuchainRecordWriterTest);
+  TDUnitX.RegisterTestFixture(TSkuchainArrayOfRecordWriterTest);
+
+
+end.
